2f8105cbf43c4a064b9b49b4775d5ff5ea42f7e9
Pavel Bliznakov <pavel.bliznakov@strypes.eu>
2014-11-28 11:17:14 +0200

    Initial commit.

---
+++ src/whirlwind/__init__.py
@@ -0,0 +1,10 @@
+"""
+Whirlwind
+
+A pluggable application server in Python
+
+@author: Pavel Bliznakov <pavel@logdaemon.eu>
+@since: 28/11/2014
+"""
+
+__all__ = ['web']
---
+++ src/whirlwind/web.py
@@ -0,0 +1,73 @@
+'''
+Created on Nov 28, 2014
+
+@author: Pavel Bliznakov <pavel@logdaemon.eu>
+'''
+
+import importlib
+import json
+import os
+import sys
+import tornado.ioloop
+import tornado.web
+
+from glob import glob
+
+class Application(tornado.web.Application):
+    """
+    Wrapper around the tornado Application class
+    You should provide plugins path for your application
+    as well as a settings dictionary.
+    """
+    def __init__(self, plugins_path='.', settings={}):
+        """
+        Constructor
+        """
+        self.plugins_path = plugins_path
+        handlers = []
+        lib_path = os.path.join(plugins_path, 'lib')
+        sys.path.append(lib_path)
+        plugins = glob(os.path.join(plugins_path, '*'))
+        for plugin_dir in plugins:
+            handlers += self.load_plugin(plugin_dir)
+        static_path = os.path.join(plugins_path, 'static')
+        templates_path = os.path.join(plugins_path, 'templates')
+        settings['static_path'] = static_path
+        settings['templates_path'] = templates_path
+        super(Application, self).__init__(self, handlers,
+                                          autoescape=None,
+                                          **settings)
+
+    def __load_modules__(self, modules_names):
+        """
+        Dynamically load modules, related to a plugin
+        """
+        modules = {}
+        for name in modules_names:
+            try:
+                modules[name] = importlib.import_module(name)
+            except ImportError:
+                sys.stderr.write("Unable to import {0}.".format(name))
+        return modules
+
+    def load_plugin(self, plugin_dir):
+        """
+        Load plugin
+        """
+        plugin_config = os.path.join(plugin_dir, 'plugin.json')
+        with open(plugin_config, 'r') as conf_fd:
+            config = json.load(conf_fd)
+            modules = self.__load_modules__(config['modules'])
+            handlers = [(r'{0}'.format(uri), getattr(modules[mod], cls_name))
+                        for (uri, [mod, cls_name]) in
+                        [(handler['uri'], handler['class'].rsplit('.', 1))
+                         for handler in config['handlers']]]
+            return handlers
+
+def run(application, port=8080, address="0.0.0.0"):
+    """
+    Run the server
+    """
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(port, address)
+    tornado.ioloop.IOLoop().instance().start()
